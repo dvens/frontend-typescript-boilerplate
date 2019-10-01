8e747b51b88fa3d42dbeff7ca30356378377ceba
dylan vens <dylan.vens@tamtam.nl>
2019-10-01 14:31:16 +0200

    added prettier + eslint

---
+++ .eslintrc.js
@@ -0,0 +1,49 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    plugins: ['simple-import-sort'],
+    env: {
+        browser: true,
+        mocha: true,
+    },
+    extends: [
+      'plugin:@typescript-eslint/recommended',
+      'plugin:prettier/recommended',
+      'plugin:jsx-a11y/recommended',
+      'plugin:import/errors',
+      'plugin:import/warnings',
+      'plugin:import/typescript',
+    ],
+    parserOptions: {
+      ecmaVersion: 2018,
+      sourceType: 'module',
+      ecmaFeatures: {
+        jsx: true,
+      },
+    },
+    rules: {
+      semi: 'off',
+      '@typescript-eslint/member-delimiter-style': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-non-null-assertion': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-use-before-define': 'off',
+      '@typescript-eslint/indent': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-object-literal-type-assertion': 'off',
+      '@typescript-eslint/explicit-member-accessibility': 'off',
+      '@typescript-eslint/camelcase': 'off',
+      '@typescript-eslint/no-empty-interface': 'off',
+      'prettier/prettier': 'off',
+      'jsx-a11y/click-events-have-key-events': 'off',
+      'import/no-unresolved': 'off',
+      'simple-import-sort/sort': 'warn',
+      'import/named': 'off',
+      'import/namespace': 'off',
+      'jsx-a11y/anchor-is-valid': 'off',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+}
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+class Test {
+    sayHi() {
+        console.log('Hello!!');
+    }
+}
