c44ceee74c3260069768cb452b7ab778f9974634
Diogo Batista <diogobv.batista@gmail.com>
2019-10-25 16:41:57 +0200

    replace readdir with readirSync

--- server/routes/webRoutes.ts
+++ server/routes/webRoutes.ts
@@ -3,29 +3,23 @@ import path from 'path';
 
 const staticViewsPath = path.join(__dirname, '../../pages/static');
 
-function getRoutes(files: string[]) {
+export function getRoutes(files: string[]) {
     return files.map((file: string) => {
         return file.substr(0, file.indexOf('.'));
     });
 }
 
-function getDirectories(path: string, callback: any) {
-    fs.readdir(path, function(err: any, content: any) {
-        if (err) return callback(err);
-        callback(null, getRoutes(content));
-    });
+export function getDirectories(path: string) {
+    return fs.readdirSync(path);
 }
 
-const webRoutes = (app: any) => {
-    getDirectories(staticViewsPath, function(err: any, routes: any) {
-        if (err) {
-            console.error(err);
-        }
+export const webRoutes = (app: any) => {
+    const files = getDirectories(staticViewsPath);
+    const routes = getRoutes(files);
 
-        routes.forEach((route: string) => {
-            app.get(`/${route}`, (_: any, res: any) => {
-                res.render(`${staticViewsPath}/${route}.html`, { project: { debug: true } });
-            });
+    routes.forEach((route: string) => {
+        app.get(`/${route}`, (_: any, res: any) => {
+            res.render(`${staticViewsPath}/${route}.html`, { project: { debug: true } });
         });
     });
 };
