714d50b42c2a1be47a926c5bd34b44c25d38eb2e
dylan vens <dylan.vens@tamtam.nl>
2019-10-25 10:33:42 +0200

    added eslint

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
-import '@static/styles/main.scss';
-import './polyfills';
-import './app';
-
 if (module.hot) {
     module.hot.accept();
 }
+import '@static/styles/main.scss';
+import './app';
+import './polyfills';
